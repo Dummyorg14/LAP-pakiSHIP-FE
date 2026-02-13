1b5bf7354cbfcd2f2dbd79c5dd912cac11d42c5a
rxxrbls <rixxamae20@gmail.com>
2026-02-13 20:54:35 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Welcome to Logistics and Parking Frontend!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
